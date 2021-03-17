a801a1b7c96a375c8e4722b6af219b3573aaaed5
Marvin <mdtensuan_dev@protonmail.com>
2021-03-17 18:01:11 +0800

    Added sample data bindings.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,33 @@ namespace CoinInventory
         public MainWindow()
         {
             InitializeComponent();
+
+            List<Position> positions = new();
+
+            Position pos1 = new(1, "BTC", 0.005, 5000, "Open");
+            Position pos2 = new(2, "BCH", 0.05, 200, "Open");
+            positions.Add(pos1);
+            positions.Add(pos2);
+
+            DG1.DataContext = positions;
         }
+    }
+    public class Position
+    {
+        public Position(int id, string ticker, double pos_amount, double hist_price, string status)
+        {
+            Id = id;
+            TickerName = ticker;
+            PositionAmount = pos_amount;
+            HistoricalPrice = hist_price;
+            Status = status;
+        }
+
+        public int Id { get; set; }
+        public string TickerName { get; set; }
+        public double PositionAmount { get; set; }
+        public double HistoricalPrice { get; set; }
+        public string Status { get; set; }
+
     }
 }
