a7bfeffef16c256772b9b732ea484a386de2db99
Marvin <mdtensuan_dev@protonmail.com>
2021-04-04 17:53:30 +0800

    Added ComboBox for Ticker.

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace CoinInventory
+namespace CoinInventoryUI
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- Position.cs
+++ Position.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CoinInventory
+namespace CoinInventoryUI
 {
     class Position
     {
